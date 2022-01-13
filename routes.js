be4bd7aaf2a375e155825eeb3938c36b442a27de
saharoori <saharlaor@gmail.com>
2022-01-13 18:49:20 +0200

    Scaffolding project structure

---
+++ routes.js
@@ -0,0 +1,41 @@
+const { builtinModules } = require("module");
+// const utils = require("./utils");
+
+// Functions
+const getAllUsers = (req, res) => {
+  res.send("getAllUsers is not Implemented");
+};
+
+const getUser = (req, res) => {
+  res.send("getUser is not Implemented");
+};
+
+const createUser = (req, res) => {
+  res.send("createUser is not Implemented");
+};
+
+const makeDeposit = (req, res) => {
+  res.send("makeDeposit is not Implemented");
+};
+
+const updateUserCredit = (req, res) => {
+  res.send("updateUserCredit is not Implemented");
+};
+
+const makeWithdraw = (req, res) => {
+  res.send("makeWithdraw is not Implemented");
+};
+
+const makeTransfer = (req, res) => {
+  res.send("makeTransfer is not Implemented");
+};
+
+module.exports = {
+  getAllUsers,
+  getUser,
+  createUser,
+  makeDeposit,
+  updateUserCredit,
+  makeWithdraw,
+  makeTransfer,
+};
---
+++ utils.js
@@ -0,0 +1,11 @@
+// Imports
+const fs = require("fs");
+
+// Constants
+const JSON_PATH = `${__dirname}/accounts.json`;
+
+// Functions
+function parseUsers() {}
+
+// Exports
+// module.exports.something = something;
