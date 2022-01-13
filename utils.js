ec874e30189b335f4adf9cf5030c84bf0609267d
saharoori <saharlaor@gmail.com>
2022-01-13 20:44:19 +0200

    Read/Write capabilities for json file

--- utils.js
+++ utils.js
@@ -5,7 +5,19 @@ const fs = require("fs");
 const JSON_PATH = `${__dirname}/accounts.json`;
 
 // Functions
-function parseUsers() {}
+function parseUsers() {
+  try {
+    const buffer = fs.readFileSync(JSON_PATH);
+    return JSON.parse(buffer);
+  } catch (e) {
+    // In case of no file return empty
+    return [];
+  }
+}
+
+function writeUsers(userArray) {
+  fs.writeFileSync(JSON_PATH, JSON.stringify(userArray));
+}
 
 // Exports
-// module.exports.something = something;
+module.exports = { parseUsers, writeUsers };
